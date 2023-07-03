a2ab569d874f9ddbc7b2f400dd3c83e205c54902
urw7rs <urw.7rs@icloud.com>
2023-07-04 01:40:19 +0900

    remove null-ls

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -31,17 +31,6 @@ require('lspconfig').pylsp.setup({
 
 lsp.setup()
 
-local null_ls = require('null-ls')
-
-null_ls.setup({
-    sources = {
-        -- Replace these with the tools you have installed
-        -- make sure the source name is supported by null-ls
-        -- https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md
-        null_ls.builtins.formatting.black,
-    }
-})
-
 -- Make sure you setup `cmp` after lsp-zero
 
 local cmp = require('cmp')
