9dc155c5abf0a499d6fbfd6f4b15224442e26f61
urw7rs <urw.7rs@icloud.com>
2023-10-01 17:11:47 +0900

    use jedi_lsp + ruff_lsp

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,33 +1,44 @@
 local lsp = require('lsp-zero').preset({})
 
 lsp.on_attach(function(client, bufnr)
+    -- see :help lsp-zero-keybindings
+    -- to learn the available actions
     lsp.default_keymaps({ buffer = bufnr })
+
+    vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, { buffer = bufnr })
+
+
     lsp.buffer_autoformat()
 end)
+
+require('mason').setup({})
+require('mason-lspconfig').setup({
+    ensure_installed = { "lua_ls", "jedi_language_server", "ruff_lsp", "clangd" },
+    handlers = {
+        lsp.default_setup,
+    },
+})
 
 -- (Optional) Configure lua language server for neovim
 local lspconfig = require('lspconfig')
 
 lspconfig.lua_ls.setup(lsp.nvim_lua_ls())
 
-lspconfig.pylsp.setup {
-    capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities()),
-    settings = {
-        pylsp = {
-            plugins = {
-                jedi_completion = {
-                    include_params = true,
-                    resolve_eagerly = true,
-                },
-                ruff = {
-                    enabled = true,
-                    extendSelect = { "I" },
-                },
-            },
-        },
-    },
+lspconfig.jedi_language_server.setup({
+    completion = {
+        resolveEagerly = true
+    }
+})
 
-}
+lspconfig.ruff_lsp.setup({
+    init_options = {
+        settings = {
+            -- Any extra CLI arguments for `ruff` go here.
+            args = {},
+        }
+    }
+})
+
 
 lsp.setup()
 
